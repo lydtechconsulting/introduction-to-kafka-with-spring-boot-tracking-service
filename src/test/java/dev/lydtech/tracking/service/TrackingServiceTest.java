30f1b36a5ef46359d6d398bad1078264c796fbcd
Rob Hemmings <rob.hemmings@lydtechconsulting.com>
2023-05-18 15:33:34 +0100

    updated following review

--- src/test/java/dev/lydtech/tracking/service/TrackingServiceTest.java
+++ src/test/java/dev/lydtech/tracking/service/TrackingServiceTest.java
@@ -22,7 +22,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
 
-public class commitTrackingServiceTest {
+public class TrackingServiceTest {
 
     private KafkaTemplate kafkaProducerMock;
     private TrackingService service;
