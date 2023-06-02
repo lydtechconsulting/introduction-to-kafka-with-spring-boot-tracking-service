42b0689e7ffeb7bb2e0bfc59b6ff2656a12f20a1
Rob Hemmings <rob.hemmings@lydtechconsulting.com>
2023-06-02 15:53:50 +0100

    renamed DispatchPreparingHandlerTest to DispatchTrackingHandlerTest

--- src/test/java/dev/lydtech/tracking/handler/DispatchTrackingHandlerTest.java
+++ src/test/java/dev/lydtech/tracking/handler/DispatchTrackingHandlerTest.java
@@ -14,7 +14,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-public class DispatchPreparingHandlerTest {
+public class DispatchTrackingHandlerTest {
 
     private TrackingService trackingServiceMock;
 
