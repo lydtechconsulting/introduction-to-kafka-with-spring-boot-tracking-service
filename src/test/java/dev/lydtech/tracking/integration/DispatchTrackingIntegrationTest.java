b65ebdb5da3f9eb821ae076f7c47bcb60f1daa5c
rgolder1 <rgolder1@yahoo.com>
2023-06-08 11:46:55 +0100

    Reverted integration test change.

--- src/test/java/dev/lydtech/tracking/integration/DispatchTrackingIntegrationTest.java
+++ src/test/java/dev/lydtech/tracking/integration/DispatchTrackingIntegrationTest.java
@@ -1,6 +1,5 @@
 package dev.lydtech.tracking.integration;
 
-import dev.lydtech.dispatch.message.DispatchCompleted;
 import dev.lydtech.dispatch.message.DispatchPreparing;
 import dev.lydtech.dispatch.message.TrackingStatusUpdated;
 import dev.lydtech.tracking.TrackingConfiguration;
@@ -24,7 +23,6 @@ import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.time.LocalDate;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -82,7 +80,7 @@ public class DispatchTrackingIntegrationTest {
     }
 
     @Test
-    public void testDispatchPreparingFlow() throws Exception{
+    public void testDispatchTrackingFlow() throws Exception{
         DispatchPreparing dispatchPreparing = TestEventData.buildDispatchPreparingEvent(randomUUID());
         sendMessage(DISPATCH_TRACKING_TOPIC, dispatchPreparing);
 
@@ -91,15 +89,6 @@ public class DispatchTrackingIntegrationTest {
 
     }
 
-    @Test
-    public void testDispatchedFlow() throws Exception{
-        DispatchCompleted dispatchCompleted = TestEventData.buildDispatchCompletedEvent(randomUUID(), LocalDate.now().toString());
-        sendMessage(DISPATCH_TRACKING_TOPIC, dispatchCompleted);
-
-        await().atMost(3, TimeUnit.SECONDS).pollDelay(100, TimeUnit.MILLISECONDS)
-                .until(testListener.trackingStatusCounter::get, equalTo(1));
-
-    }
     private void sendMessage(String topic, Object data) throws Exception {
         kafkaTemplate.send(MessageBuilder
                 .withPayload(data)
