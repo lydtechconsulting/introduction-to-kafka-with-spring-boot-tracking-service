c1687a987d5e02dcc88a48005f64ae101754fa7a
rgolder1 <rgolder1@yahoo.com>
2023-06-29 12:59:33 +0100

    KafkaHandler addition

--- src/main/java/dev/lydtech/tracking/handler/DispatchTrackingHandler.java
+++ src/main/java/dev/lydtech/tracking/handler/DispatchTrackingHandler.java
@@ -5,23 +5,25 @@ import dev.lydtech.tracking.service.TrackingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
+@KafkaListener(
+        id = "dispatchTrackingConsumerClient",
+        topics = "dispatch.tracking",
+        groupId = "tracking.dispatch.tracking",
+        containerFactory = "kafkaListenerContainerFactory"
+)
 public class DispatchTrackingHandler {
 
     @Autowired
     private final TrackingService trackingService;
 
-    @KafkaListener(
-            id = "dispatchTrackingConsumerClient",
-            topics = "dispatch.tracking",
-            groupId = "tracking.dispatch.tracking",
-            containerFactory = "kafkaListenerContainerFactory"
-    )
+    @KafkaHandler
     public void listen(DispatchPreparing dispatchPreparing) throws Exception {
         try {
             trackingService.process(dispatchPreparing);;
